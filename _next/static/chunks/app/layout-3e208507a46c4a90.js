19c310adee4d64b09a032008611f78e2450b9368
pppontus <77966355+pppontus@users.noreply.github.com>
2025-01-14 13:59:10 +0100

    added /

--- _next/static/chunks/app/layout-3e208507a46c4a90.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5904:function(n,e,u){Promise.resolve().then(u.t.bind(u,9763,23))},9763:function(){}},function(n){n.O(0,[85,971,117,744],function(){return n(n.s=5904)}),_N_E=n.O()}]);
